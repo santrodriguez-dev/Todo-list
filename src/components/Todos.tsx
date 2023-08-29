a1d66f568145a77a7b1db9923cee046ff5f8a732
Santiago Rodríguez <srodriguezm95@gmail.com>
2023-08-28 22:21:27 -0500

    animaciones en interacciones con lista

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,13 @@
 import { useTodosContext } from '../contexts/todo'
 import { Todo } from './Todo'
 
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+
 export const Todos: React.FC = () => {
+  const [animationParent] = useAutoAnimate()
   const { todos } = useTodosContext()
   return (
-    <ul>
+    <ul ref={animationParent}>
       {todos.map(todo => (
         <li key={todo.id}>
           <Todo
